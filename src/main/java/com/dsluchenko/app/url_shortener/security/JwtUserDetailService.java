4f4066372f5e436d9c367a8e2156a34e3a3985e5
DSLuchenko <DSLuchenko@bk.ru>
2023-03-21 13:35:26 +0300

    refactor: 1.Handle UserNotFoundAuthenticationException;

--- src/main/java/com/dsluchenko/app/url_shortener/security/JwtUserDetailService.java
+++ src/main/java/com/dsluchenko/app/url_shortener/security/JwtUserDetailService.java
@@ -1,6 +1,7 @@
 package com.dsluchenko.app.url_shortener.security;
 
 import com.dsluchenko.app.url_shortener.entity.User;
+import com.dsluchenko.app.url_shortener.exception.authenticationException.UserNotFoundAuthenticationException;
 import com.dsluchenko.app.url_shortener.security.jwt.JwtUserFactory;
 import com.dsluchenko.app.url_shortener.service.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,11 +20,11 @@ public class JwtUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.findByLogin(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User " + username + "not found");
+        try {
+            User user = userService.findByLogin(username);
+            return JwtUserFactory.create(user);
+        } catch (UserNotFoundAuthenticationException ex) {
+            throw new UsernameNotFoundException(ex.getMessage());
         }
-
-        return JwtUserFactory.create(user);
     }
 }
